75cba5b61155b6a747b9cd75728f381174bf6f6b
Magesh <tkmagesh77@gmail.com>
2017-04-28 12:12:18 +0530

    refactored again

--- Day-03/01-PubSub/radio.js
+++ Day-03/01-PubSub/radio.js
@@ -4,38 +4,38 @@ var radio = (function(){
 		this._subscriptions = [];
 	}
 	
+	function subscribeOne(subscription){
+		if (!(typeof subscription === 'function' || Array.isArray(subscription))) return;
+		this._subscriptions = this._subscriptions || [];
+		this._subscriptions.push(subscription);
+	}
 	Radio.prototype.subscribe = function (){
-		var self = this;
-		Array.prototype.forEach.call(arguments, function(subscription){
-			if (!(typeof subscription === 'function' || Array.isArray(subscription))) return;
-			self._subscriptions = self._subscriptions || [];
-			self._subscriptions.push(subscription);
-		});
+		Array.prototype.forEach.call(arguments, subscribeOne.bind(this));
 		return this;
 	}
 
+	function broadcastOne(subscription){
+		var fn = subscription,
+			context = this;
+		if (Array.isArray(subscription)){
+			fn = subscription[0];
+			context = subscription[1];
+		} 
+		fn.call(context, data);
+	}
 	Radio.prototype.broadcast = function (data){
 		var subscriptionFns = this._subscriptions || [];
-		subscriptionFns.forEach(function(subscription){
-			var fn = subscription,
-				context = this;
-			if (Array.isArray(subscription)){
-				fn = subscription[0];
-				context = subscription[1];
-			} 
-			fn.call(context, data);
-		});
+		subscriptionFns.forEach(broadcastOne);
 		return this;
 	}
-
-	Radio.prototype.unsubscribe = function (){
-		var self = this;
-		Array.prototype.forEach.call(arguments, function(subscription){
-			self._subscriptions = self._subscriptions.filter(function(sc){
-				subscriptionFn = Array.isArray(sc) ? sc[0] : sc;
-				return subscriptionFn !== subscription;
-			});
+	function unsubscribeOne(subscription){
+		this._subscriptions = this._subscriptions.filter(function(sc){
+			subscriptionFn = Array.isArray(sc) ? sc[0] : sc;
+			return subscriptionFn !== subscription;
 		});
+	}
+	Radio.prototype.unsubscribe = function (){
+		Array.prototype.forEach.call(arguments, unsubscribeOne.bind(this));
 		return this;
 	}
 
